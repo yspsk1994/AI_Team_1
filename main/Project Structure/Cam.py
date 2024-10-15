119c43611f800e896ea519ebee0715266eb52819
YOON TAE JOON <yspsk1994@naver.com>
2024-10-15 15:09:48 +0900

    revise MT_Cam and Cam 241015

--- main/Project Structure/Cam.py
+++ main/Project Structure/Cam.py
@@ -1,37 +1,88 @@
 import threading
 import queue
 import cv2
-import shared
+# import shared
+import time
+from multiprocessing import shared_memory
+ 
+ 
+class Cam1_Msg(threading.Thread):
+    def __init__(self,shared_queue):
+        super().__init__()
+        self.running = True
+        self.cam_shared_queue = shared_queue
+        
+        
+    def run(self):
+        while self.running:
+            # if not self.mt_cam.mt_cam1_que.empty():
+                # message, sender = self.mt_cam.mt_cam1_que.get()
+            continue
+    def stop(self):
+        self.running = False
+        
+
 
 class Cam1(threading.Thread):
     def __init__(self):
         super().__init__()
         self.running = True
-        self.mt_cam = shared.mt_cam_instance
+        self.cap = cv2.VideoCapture(1)
 
+        # self.width = 1280
+        # self.height = 720
+        self.cam1_que = queue.Queue()
+        
     def run(self):
         while self.running:
-            if not self.mt_cam.mt_cam1_que.empty():
-                message, sender = self.mt_cam.mt_cam1_que.get()
-                
-
+            # time.sleep(1000)
+            ret, frame = self.cap.read()
+            if ret:
+                self.cam1_que.put(frame)
+                cv2.imshow("cam1_frmae", frame)
+                cv2.waitKey(1)
+         
+            
+            
+            
     def stop(self):
         self.running = False
+    
 
 
+
+class Cam2_Msg(threading.Thread):
+    def __init__(self,shared_queue):
+        super().__init__()
+        self.running = True
+        self.cam_shared_queue = shared_queue
+    
+        # self.mt_cam = shared.mt_cam_instance
+        
+    def run(self):
+        while self.running:
+            if not self.cam_shared_queue.empty():
+                message, sender = self.cam_shared_queue.get()
+            continue
+                    
+    def stop(self):
+        self.running = False
+        
 
 
 class Cam2(threading.Thread):
     def __init__(self):
         super().__init__()
         self.running = True
-        self.mt_cam = shared.mt_cam_instance
-
+        # self.cap = cv2.VideoCapture(0)
+        self.width = 1280
+        self.height = 720
+        self.cam2_que = queue.Queue()
+        
     def run(self):
         while self.running:
-            if not self.mt_cam.mt_cam2_que.empty():
-                message, sender = self.mt_cam.mt_cam2_que.get()
-                
-
+            # ret, frame = self.cap.read(1)
+            # self.cam2_que.put(frame)
+            continue
     def stop(self):
         self.running = False
--- main/Project Structure/SmartLibrary.py
+++ main/Project Structure/SmartLibrary.py
@@ -1,7 +1,7 @@
 from ConcreteMediator import ConcreteMediator
 from MT_Cam import MT_Cam
 from MT_Function import MT_Function
-
+import queue
 import time
 
 class SmartLibrary:
@@ -11,8 +11,9 @@ class SmartLibrary:
 
 
 mediator = ConcreteMediator()
+shared_queue = queue.Queue() 
+main_cam_thread = MT_Cam(mediator, "Cam_Thread", shared_queue = shared_queue)
 
-main_cam_thread = MT_Cam(mediator, "Cam_Thread")
 main_function_thread = MT_Function(mediator, "Function_Thread")
 
 mediator.add_user(main_cam_thread)
@@ -20,3 +21,4 @@ mediator.add_user(main_function_thread)
 
 main_cam_thread.start()
 main_function_thread.start()
+
